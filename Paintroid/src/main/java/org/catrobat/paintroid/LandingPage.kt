2b12fba0ac9d6a4e0a037e2d0c569bb2dd4d5d32
Rd <rd4ramadevi@gmail.com>
2023-05-16 11:34:30 +0530

    Created Landing Page Activity

---
+++ Paintroid/src/main/java/org/catrobat/paintroid/LandingPage.kt
@@ -0,0 +1,11 @@
+package org.catrobat.paintroid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LandingPage: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pocketpaint_landing_page)
+    }
+}
