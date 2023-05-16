7f8002cb3bb327363aa3e266854f33b00fb2d5eb
Rd <rd4ramadevi@gmail.com>
2023-05-16 13:51:54 +0530

    Landing Page set as Launcher Activity

--- Paintroid/src/main/java/org/catrobat/paintroid/LandingPage.kt
+++ Paintroid/src/main/java/org/catrobat/paintroid/LandingPage.kt
@@ -1,11 +1,19 @@
 package org.catrobat.paintroid
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class LandingPage: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pocketpaint_landing_page)
+
+        val landingPageBtn = findViewById<Button>(R.id.btn_pocket_paint_landing_page_btn)
+        landingPageBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
